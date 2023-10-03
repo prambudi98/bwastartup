19cd26d0abeff86acb4f95f1021513aac8999340
Prambudi Setiyadi <prambudi.setiyadi@mnclife.com>
2023-10-03 15:06:05 +0700

    init project register

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bwastartup/user"
+	"bwastartup/user/handler"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(localhost:3306)/bwastartup?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	api.POST("/users", userHandler.RegisterUser)
+	router.Run()
+}
