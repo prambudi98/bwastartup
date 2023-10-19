682fb01b0b5bcd1e44b6738f9c84b3dd504d04aa
Prambudi Setiyadi <prambudi.setiyadi@mnclife.com>
2023-10-19 14:59:35 +0700

    jwt token

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bwastartup/auth"
 	"bwastartup/helper"
 	"bwastartup/user"
 	"fmt"
@@ -11,10 +12,11 @@ import (
 
 type userHandler struct {
 	userService user.Service
+	authService auth.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler {
-	return &userHandler{userService}
+func NewUserHandler(userService user.Service, authService auth.Service) *userHandler {
+	return &userHandler{userService, authService}
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context) {
@@ -34,7 +36,14 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-	formatter := user.FormatUser(newUser, "tokentokentokentokentokentoken")
+
+	token, err := h.authService.GenerateToken(newUser.Id)
+	if err != nil {
+		response := helper.APIResponse("Generate Token Failed ", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	formatter := user.FormatUser(newUser, token)
 	response := helper.APIResponse("Account has been registered ", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 
@@ -59,7 +68,13 @@ func (h *userHandler) Login(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-	formatter := user.FormatUser(loggedinUser, "tokentokentokentokentokentoken")
+	token, err := h.authService.GenerateToken(loggedinUser.Id)
+	if err != nil {
+		response := helper.APIResponse("Generate Token Failed ", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	formatter := user.FormatUser(loggedinUser, token)
 	response := helper.APIResponse("SuccessFully Login", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bwastartup/auth"
 	"bwastartup/handler"
 	"bwastartup/user"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -20,7 +22,10 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
-	userHandler := handler.NewUserHandler(userService)
+	authService := auth.NewService()
+	userHandler := handler.NewUserHandler(userService, authService)
+
+	fmt.Println(authService.GenerateToken(1001))
 	/*
 		input := user.LoginInput{
 			Email:    "Pos2w@gmail.com",
